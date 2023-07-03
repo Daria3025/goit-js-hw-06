026342d1a450fd542715b2f585984c590231a6ae
Daria3025 <dtimofeeva191@icloud.com>
2023-07-03 23:47:34 +0300

    task09

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,13 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color');
+const body = document.querySelector('body');
+const colorName = document.querySelector('.color');
+buttonEl.addEventListener('click', ChangeColor);
+function ChangeColor(evt) {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  colorName.textContent = randomColor;
+}
