125e046bf716d6c11d689ab9fec7c16a4151479a
Daria3025 <dtimofeeva191@icloud.com>
2023-07-04 00:35:06 +0300

    task10

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,28 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const numberInput = document.querySelector('input');
+const createBtn = document.querySelector('[data-create]');
+const destroyBtn = document.querySelector('[data-destroy]');
+const boxesEl = document.querySelector('#boxes');
+
+createBtn.addEventListener('click', createBoxes);
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  amount = numberInput.value;
+  const boxSize = 30;
+  let html = '';
+  for (let i = 0; i < amount; i += 1) {
+    const size = boxSize + i*10;
+    const color = getRandomHexColor();
+    html += `<div style="width:${size}px; height:${size}px; background-color:${color}"></div>`;
+  }
+  boxesEl.innerHTML = html;
+}
+
+function destroyBoxes() {
+  boxesEl.innerHTML = '';
+  numberInput.value = '';
+}
