5e5715e3572385a73002c572e8a307c954546780
Daria3025 <dtimofeeva191@icloud.com>
2023-07-03 23:32:32 +0300

    task08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener("submit", OnSubmit);
+function OnSubmit(evt) {
+    evt.preventDefault();
+    const { email, password } = evt.currentTarget.elements;
+    if (!email.value || !password.value) {
+        alert('All fields must be filled!')
+    } else {
+        const data = {
+        email: email.value,
+        password: password.value,
+    };
+        console.dir(data);
+    }
+    evt.currentTarget.reset();
+}
